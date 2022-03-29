00bbb267c96665b247d22672f2d37d8459de72db
github username <github email address>
2022-03-28 23:14:04 -0400

    line method solution

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+# Write your code here
+
+def line (array)
+    if array.length==0
+        puts "The line is currently empty."
+    else
+        new_array=[]
+        array.each_with_index do |element,idx|
+            new_array.push("#{idx+1}. #{element}")
+        end
+        puts "The line is currently: #{new_array.join(" ")}"
+    end
+end
+
+def take_a_number
+    
+end
