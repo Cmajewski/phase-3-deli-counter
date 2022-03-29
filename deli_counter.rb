172fd51471241bc1346945d73edcb4a79e61acf5
github username <github email address>
2022-03-28 23:50:22 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,12 @@ def take_a_number(deli,person)
     deli<<person
     puts "Welcome, #{person}. You are number #{deli.index(person)+1} in line."
 end
+
+def now_serving (deli)
+    if deli.length==0
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{deli[0]}."
+        deli.shift
+    end
+end
