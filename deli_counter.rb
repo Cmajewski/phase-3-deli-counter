6dbaa33b2c09bb8cc9ee82740bf6ab342e8f66b2
github username <github email address>
2022-03-28 23:46:28 -0400

    take_a_number solution

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,18 @@
 # Write your code here
 
-def line (array)
-    if array.length==0
+def line (deli)
+    if deli.length==0
         puts "The line is currently empty."
     else
-        new_array=[]
-        array.each_with_index do |element,idx|
-            new_array.push("#{idx+1}. #{element}")
+        new_deli=[]
+        deli.each_with_index do |element,idx|
+            new_deli.push("#{idx+1}. #{element}")
         end
-        puts "The line is currently: #{new_array.join(" ")}"
+        puts "The line is currently: #{new_deli.join(" ")}"
     end
 end
 
-def take_a_number
-    
+def take_a_number(deli,person)
+    deli<<person
+    puts "Welcome, #{person}. You are number #{deli.index(person)+1} in line."
 end
